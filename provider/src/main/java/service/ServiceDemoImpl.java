41d9bc5523b0e95ea05209738cd03c09a55bac80
WangShiXiang <wangshixiang1020@gmail.com>
2018-05-04 12:49:07 +0800

    重写快速排序，使代码更容易理解

--- provider/src/main/java/service/ServiceDemoImpl.java
+++ provider/src/main/java/service/ServiceDemoImpl.java
@@ -9,7 +9,8 @@ public class ServiceDemoImpl implements ServiceDemo{
     }
 
     public int[] sort(int[] arr) {
-        quickSort(arr,0,arr.length-1);
+        sort(arr,0,arr.length-1);
+        quickSort(null,2,1);//瞎写，这一行不用理会
         return arr;
     }
 
@@ -32,5 +33,29 @@ public class ServiceDemoImpl implements ServiceDemo{
         }
         quickSort(arr,left,i-1);
         quickSort(arr,i+1,right);
+    }
+    private void sort(int[] arr,int left,int right){
+        if (left>=right||arr==null||arr.length==0) return;
+        int i=left+1;
+        int j=right;
+        int key=arr[left];
+        while (i<j){
+            while (j>i&&arr[j]>=key){
+                j--;
+            }
+            while (i<j&&key>=arr[i]){
+                i++;
+            }
+            swap(arr,i,j);
+        }
+        arr[left]=arr[i];
+        arr[i]=key;
+        sort(arr,left,i-1);
+        sort(arr,i+1,right);
+    }
+    private void swap(int[] arr,int i,int j){
+        int key=arr[i];
+        arr[i]=arr[j];
+        arr[j]=key;
     }
 }
